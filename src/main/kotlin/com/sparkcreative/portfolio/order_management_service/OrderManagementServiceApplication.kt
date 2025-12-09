a7b800e8c36eac68ffadddf60e161087d65f186b
ogatashogo-spark-creative <ogata.shogo@spark-creative.co.jp>
2025-12-09 18:09:44 +0900

    feat: Initial setup for Kotlin Spring Boot project

---
+++ src/main/kotlin/com/sparkcreative/portfolio/order_management_service/OrderManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sparkcreative.portfolio.order_management_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderManagementServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderManagementServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/sparkcreative/portfolio/order_management_service/OrderManagementServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sparkcreative.portfolio.order_management_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderManagementServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
